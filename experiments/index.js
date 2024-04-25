6df4ce68e5a1ccf6a996f5325d946682182f4699
MrPanda376 <luca.silvestrin2008@gmail.com>
2024-04-25 23:29:55 +0200

    tested embeds and select menus

--- experiments/index.js
+++ experiments/index.js
@@ -1,8 +1,5 @@
-const { Client, GatewayIntentBits, Interaction, ModalSubmitInteraction, EmbedBuilder } = require("discord.js");
+const { Client, GatewayIntentBits, ActionRowBuilder, StringSelectMenuBuilder, EmbedBuilder, ComponentType } = require("discord.js");
 const { token } = require('../config/config.json');
-const { saveDataToFile, searchNameInFile, findValue, sleep } = require('./functions');
-const { altFindValue, altSearchNameInFile } = require('./altFunction')
-const fs = require('fs');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -10,65 +7,41 @@ client.once('ready', () => {
     console.log('Il bot è online!');
 });
 
-// COMANDI
-
-
-
-
-
-
 //TEST EMBEDS
 
-const exampleEmbed = {
-    color: 0xff7300,
-    title: 'Some title',
-    url: 'https://discord.js.org',
-    author: {
-        name: 'Some name',
-        icon_url: 'https://i.imgur.com/AfFp7pu.png',
-        url: 'https://discord.js.org',
-    },
-    description: 'Some description here',
-    thumbnail: {
-        url: 'https://i.imgur.com/AfFp7pu.png',
-    },
-    fields: [
-        {
-            name: 'Regular field title',
-            value: 'Some value here',
-        },
-        {
-            name: '\u200b',
-            value: '\u200b',
-            inline: false,
-        },
-        {
-            name: 'Inline field title',
-            value: 'Some value here',
-            inline: true,
-        },
-        {
-            name: 'Inline field title',
-            value: 'Some value here',
-            inline: true,
-        },
-        {
-            name: 'Inline field title',
-            value: 'Some value here',
-            inline: true,
-        },
-    ],
-    image: {
-        url: 'https://i.imgur.com/AfFp7pu.png',
-    },
-    timestamp: new Date().toISOString(),
-    footer: {
-        text: 'Some footer text here',
-        icon_url: 'https://i.imgur.com/AfFp7pu.png',
-    },
-};
+const exampleEmbed = new EmbedBuilder()
+	.setColor(0xff7300)
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setTimestamp()
+	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
 
+
+
+
+
+const selectMenu = new StringSelectMenuBuilder()
+    .setCustomId('select')
+    .setPlaceholder('Select an option')
+    .addOptions([
+        { label: 'Option 1', value: 'option1' },
+        { label: 'Option 2', value: 'option2' },
+        { label: 'Option 3', value: 'option3', description: 'prova' }
+]);
+
+const row = new ActionRowBuilder().addComponents(selectMenu);
 
 
 
@@ -81,17 +54,26 @@ client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
 
     const { commandName } = interaction;
-    const options = interaction.options;
     const channel_CMD = await client.channels.fetch('1228448453672046722');
+    let selected
 
-    if (commandName === 'prova') {
-        await interaction.reply('Prova eseguita con successo!');
-        
-        //channel_CMD.send({ embeds: [exampleEmbed] });
+    if (commandName === 'start_tracker') {
+        const reply = await interaction.reply({ embeds: [exampleEmbed], components: [row] });
 
-    } else if (commandName === 'save-now') {
-        await interaction.reply('Variabili salvate con successo!');
-        manualSave();
+        const collector = reply.createMessageComponentCollector({
+            componentType: ComponentType.StringSelect,
+            time: 10000,
+        });
+
+        collector.on('collect', (interaction) => {
+            interaction.reply(`You selected: ${interaction.values[0]}`);
+            selected = interaction.values[0];
+        });
+
+        collector.on('end', () => {
+            reply.edit({ components: [] });
+            console.log(selected);
+        });
     }
 });
 
